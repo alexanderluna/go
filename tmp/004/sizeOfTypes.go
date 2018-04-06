6cd9d088dec9c5d12a99383f9d7cccc9e99097b1
Mycroft1891 <mycroft1891@gmail.com>
2018-04-06 20:45:22 +0200

    use dot import

--- tmp/004/sizeOfTypes.go
+++ tmp/004/sizeOfTypes.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"unsafe"
+	. "fmt"
+	. "unsafe"
 )
 
 func main() {
-	fmt.Printf("%T,\t size of: %d\n", int(10), unsafe.Sizeof(int(10)))
-	fmt.Printf("%T,\t size of: %d\n", int8(10), unsafe.Sizeof(int8(10)))
-	fmt.Printf("%T,\t size of: %d\n", int16(10), unsafe.Sizeof(int16(10)))
-	fmt.Printf("%T,\t size of: %d\n", int32(10), unsafe.Sizeof(int32(10)))
-	fmt.Printf("%T,\t size of: %d\n", int64(10), unsafe.Sizeof(int64(10)))
-	fmt.Printf("%T,\t size of: %d\n", string("a"), unsafe.Sizeof(string("a")))
-	fmt.Printf("%T,\t size of: %d\n", rune(97), unsafe.Sizeof(rune(97)))
+	Printf("%T,\t size of: %d\n", int(10), Sizeof(int(10)))
+	Printf("%T,\t size of: %d\n", int8(10), Sizeof(int8(10)))
+	Printf("%T,\t size of: %d\n", int16(10), Sizeof(int16(10)))
+	Printf("%T,\t size of: %d\n", int32(10), Sizeof(int32(10)))
+	Printf("%T,\t size of: %d\n", int64(10), Sizeof(int64(10)))
+	Printf("%T,\t size of: %d\n", string("a"), Sizeof(string("a")))
+	Printf("%T,\t size of: %d\n", rune(97), Sizeof(rune(97)))
 }
