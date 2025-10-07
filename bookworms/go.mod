236b8c4997971a06ff5d26927f14e7e71e13a5b6
Alexander Luna <alexanderluna1891@gmail.com>
2025-10-07 21:37:03 +0200

    Refactor

--- bookworms/go.mod
+++
@@ -1,3 +0,0 @@
-module bookworms
-
-go 1.25.1
--- bookworms/main_internal_test.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func Example_main() {
-	main()
-	// Output:
-	// Here are the common books:
-	// - The Handmaid's Tale by Margaret Atwood
-}
